784990419cf9cc6de4052a8a17c2429c8acf3377
JohnKberg <john.kberg81@gmail.com>
2016-09-30 16:54:14 +0200

    Added AngularJS + started on Angular UI for User management

--- yaBookingSystem/FrontEnd/angularjs/users-controller.js
+++ yaBookingSystem/FrontEnd/angularjs/users-controller.js
@@ -1,1 +1,23 @@
-﻿
+﻿var app = angular.module('UsersApp', []);
+
+app.controller('UsersCtrl', function ($scope, $html) {
+
+    $scope.users = [];
+    
+    $scope.listUsers = function () {
+
+        $scope.users = [];
+
+        $http.get("/api/Users").success(function (data, status, headers, config) {
+
+            $scope.users = data;
+
+            //TODO: Add code here...
+
+        }).error(function (data, status, headers, config) {
+
+            //TODO: Add code here...
+
+        });
+    };
+});
